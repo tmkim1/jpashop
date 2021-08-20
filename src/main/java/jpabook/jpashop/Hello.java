18007c647ba9e850154ae967b6c05ea6340b65c5
kimtaemin <xoalstjtm@gmail.com>
2021-08-20 22:17:30 +0900

    1. 편리한 화면 반영을 위한 devtools dependencies 추가 2. 타임 리프 작동 확인

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter @Getter
+public class Hello {
+    private String data;
+}
--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+
+		hello.setData("test");
+
+		String data = hello.getData();
+		System.out.println(data);
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
