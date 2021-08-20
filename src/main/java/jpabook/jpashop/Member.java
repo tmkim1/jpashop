471627aca5822c5a7d88261d4305fd6b65d28adf
kimtaemin <xoalstjtm@gmail.com>
2021-08-20 23:54:22 +0900

    1. application.properties -> application.yml로 변경 2. Member save, find JPA 및 JUnit Test

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Setter @Getter
+public class Member {
+
+    @Id @GeneratedValue
+    private long id;
+    private String userName;
+
+}
