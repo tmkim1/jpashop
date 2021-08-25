f314115aea4f1d09a9452807bdbac484a8139fc1
tmkim1 <xoalstjtm@gmail.com>
2021-08-25 17:48:05 +0900

    회원 관련 기능 테스트 작성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복확인() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+        //when
+        memberService.join(member1);
+        memberService.join(member2); //예외가 발생해야 함.
+
+//        @Test(expected = IllegalStateException.class) -> 해당 어노테이션 설정으로 인해 아래 코드와 같은 기능을 수행
+//        try {
+//            memberService.join(member2); //예외가 발생해야 함.
+//        } catch (IllegalStateException e) {
+//            return;
+//        }
+
+        //then
+        fail("예외가 발생해야 한다."); // 해당 fail 지점에 오면 안 됨.
+
+    }
+
+}
