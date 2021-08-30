b03ec08ba74a5f7f12f5214f55324a4ef91ffce3
tmkim1 <xoalstjtm@gmail.com>
2021-08-30 22:49:10 +0900

    상품 등록 및 목록 조회 구현

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -11,7 +11,6 @@ public class MemberForm {
 
     @NotEmpty(message = "회원 이름은 필수 입니다.")
     private String name;
-
     private String city;
     private String street;
     private String zipcode;
